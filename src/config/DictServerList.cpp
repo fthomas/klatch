7af970e553fd83b32def945ce171f430068a2c24
Frank S. Thomas <fst@debian.org>
2012-03-13 00:24:33 +0100

    Added more methods to DictServerList.

--- src/config/DictServerList.cpp
+++ src/config/DictServerList.cpp
@@ -22,13 +22,40 @@
 #include <QVariant>
 #include <KConfig>
 #include <KConfigGroup>
+#include <KLocalizedString>
 #include "config/DictServerItem.h"
 
 DictServerList::DictServerList(KConfig* config, QObject* parent)
     : QAbstractListModel{parent}, config_{config} {
+  readConfig();
 }
 
-QVariant DictServerList::data(const QModelIndex&, int) const {
+QVariant DictServerList::data(const QModelIndex& index, int role) const {
+  if (!index.isValid() || index.row() >= servers_.size()) {
+    return QVariant{};
+  }
+
+  if (role == Qt::DisplayRole && index.column() < 2) {
+    const DictServerItem& server = servers_.at(index.row());
+
+    switch (index.column()) {
+      case 0: return server.hostName();
+      case 1: return server.port();
+    }
+  }
+  return QVariant{};
+}
+
+QVariant DictServerList::headerData(int section, Qt::Orientation orientation,
+                                    int role) const {
+  if (orientation != Qt::Horizontal || role != Qt::DisplayRole) {
+    return QVariant{};
+  }
+
+  switch (section) {
+    case 0: return i18n("Hostname");
+    case 1: return i18n("Port");
+  }
   return QVariant{};
 }
 
@@ -40,17 +67,43 @@ int DictServerList::columnCount(const QModelIndex&) const {
   return 2;
 }
 
+bool DictServerList::insertRow(const DictServerItem& server) {
+  KConfigGroup subgroup =
+    config_->group(dict_name_).group(newSubgroupName());
+
+  DictServerItem my_server = server;
+  my_server.setConfigGroup(subgroup);
+
+  const int new_row = servers_.size();
+  beginInsertRows(QModelIndex{}, new_row, new_row);
+  servers_ << my_server;
+  endInsertRows();
+
+  return true;
+}
+
 void DictServerList::readConfig() {
   servers_.clear();
 
-  const KConfigGroup dict_group = config_->group("Dict");
+  KConfigGroup dict_group = config_->group(dict_name_);
   if (!dict_group.exists()) return;
 
   const QStringList subgroups = dict_group.groupList();
   for (const QString& subgroup : subgroups) {
-    if (subgroup.startsWith("Server")) {
-      const KConfigGroup server_group = dict_group.group(subgroup);
-      servers_ << DictServerItem(server_group);
+    if (subgroup.startsWith(server_prefix_)) {
+      servers_ << DictServerItem(dict_group.group(subgroup));
     }
   }
+}
+
+QString DictServerList::newSubgroupName() const {
+  KConfigGroup dict_group = config_->group(dict_name_);
+
+  QString name;
+  int i = 0;
+
+  do {
+    name = QString{"%1%2"}.arg(server_prefix_).arg(i++);
+  } while (dict_group.hasGroup(name));
+  return name;
 }
--- src/config/DictServerList.h
+++ src/config/DictServerList.h
@@ -18,16 +18,24 @@ class DictServerList : public QAbstractListModel {
   explicit DictServerList(KConfig* config, QObject* parent = 0);
 
   QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+  QVariant headerData(int section, Qt::Orientation orientation,
+                      int role = Qt::DisplayRole) const;
 
   int rowCount(const QModelIndex& parent = QModelIndex{}) const;
   int columnCount(const QModelIndex& parent = QModelIndex{}) const;
 
+  bool insertRow(const DictServerItem& server);
+
  private:
   void readConfig();
+  QString newSubgroupName() const;
 
  private:
   KConfig* config_;
   QList<DictServerItem> servers_;
+
+  const QString dict_name_ = "Dict";
+  const QString server_prefix_ = "Server";
 };
 
 #endif // CONFIG_DICTSERVERLIST_H
