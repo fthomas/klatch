ccc7bfc868e9895d9146782152902f875df5207c
Frank S. Thomas <fst@debian.org>
2012-05-18 17:19:14 +0200

    Added simple print script.

---
+++ src/scripts/print.js
@@ -0,0 +1,14 @@
+var action = new Object;
+
+action.text = "Print Selected Results";
+
+action.run = function() {
+  print("Selected results: " + results.length + "\n");
+
+  for (var i = 0; i < results.length; ++i) {
+    var res = results[i];
+    print("Word: " + res.word);
+    print("Database: " + res.database + ", " + res.databaseDescription);
+    print("Text: " + res.text);
+  }
+}
